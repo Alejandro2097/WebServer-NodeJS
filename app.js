df102eeec9f6029a02878c802af9e595f2679337
Alejandro2097 <alejo12381@hotmail.es>
2021-09-12 23:06:51 -0500

    domingo

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const http = require('http');
 
 
 http.createServer((req, res) =>{
-    res.write('Hola mundo');
+    console.log(req);
+
+    //res.writeHead(200, {'Content-Type' : 'application/json'});
+    res.setHeader('Content-Disposition', 'attachmen; filename=lista.csv')
+    res.writeHead(200, {'Content-Type' : 'application/csv'});
+    const persona = {
+        id: 1,
+        nombre: 'Alejandro'
+    }
+
+    res.write('id, nombre\n');
+    res.write('1, Fernando\n');
+    res.write('2, Maria\n');
+    res.write('3, Juan');
+    res.write('4,  Pedro');
     res.end();
 })
 .listen(3200);
