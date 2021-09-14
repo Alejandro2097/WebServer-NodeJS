278271975750b5ef3cd396e4ac9958db7eb5616e
Alejandro2097 <alejo12381@hotmail.es>
2021-09-13 19:52:39 -0500

    template

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const port = 3000
 //Servir contenido estatico 
 app.use(express.static('public'));
 
-
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+ 
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname + '/public/elements.html');
+});
  
 app.get('/hola-mundo', function (req, res) {
     res.send('Hola mundo en su respectiva ruta.')
